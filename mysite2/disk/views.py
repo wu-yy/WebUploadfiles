afc17fc79aee945b6630c461bf330dcf8358955e
蓝鲸123 <wu-yy13@mails.tsinghua.edu.cn>
2016-10-09 09:51:51 +0800

    Add files via upload

---
+++ mysite2/disk/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, render_to_response
+from django import forms
+from django.http import HttpResponse
+from disk.models import User
+
+# Create your views here.
+class UserForm(forms.Form):
+    username = forms.CharField()
+    headImg = forms.FileField()
+
+
+def register(request):
+
+    if request.method == "POST":
+        uf = UserForm(request.POST, request.FILES)
+        if uf.is_valid():
+            username=uf.cleaned_data['username']
+            headImg=uf.cleaned_data['headImg']
+            #写入数据库
+            user=User()
+            user.name=username
+            user.headImg=headImg
+            user.save()
+            return HttpResponse('upload ok!')
+    else:
+        uf = UserForm()
+
+    return render_to_response('register.html', {'uf': uf})
